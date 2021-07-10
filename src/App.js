902858dab8bbbc6dcde7933e3dc83d191454710b
harsha9726 <harsha9726@gmail.com>
2021-07-10 11:34:48 +0530

    re-staged for re-learning

--- src/App.js
+++ src/App.js
@@ -1,42 +1,9 @@
-import React, { Component } from "react";
-import "./App.css";
-import UserInput from "./UserInput/UserInput";
-import UserOutput from "./UserOutput/UserOutput";
-
-class App extends Component {
-  state = {
-    username: [
-      {name: "John"},
-      {name: "Doe"},
-      {name: "Rock"}
-    ],
-    paragraph:'None'
-  }
-  paragraphChangeHandler = (event) => {
-    this.setState(
-      { username: [
-        {name: event.target.value},
-        {name: event.target.value},
-        {name: event.target.value}
-      ],
-        paragraph: event.target.value }
-    )
-  }
-  render() {
-    return (
-      <div className="App">
-        <UserInput
-          username={this.state.username[0].name}
-          changed={this.paragraphChangeHandler}/>
-        <UserOutput
-          username={this.state.username[1].name}
-          paragraph={this.state.paragraph}/>
-        <UserOutput
-          username={this.state.username[2].name}
-          paragraph={this.state.paragraph}/>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+    </div>
+  );
 }
 
 export default App;
