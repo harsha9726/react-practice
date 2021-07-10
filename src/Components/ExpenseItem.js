dd3ecdb8321373435a10ce2c14c168f648f166e0
harsha9726 <harsha9726@gmail.com>
2021-07-10 14:14:43 +0530

    Added Exppenses component

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -5,7 +5,7 @@ const ExpenseItem = (props) => {
 
   return (
   <div className="expense-item">
-    <ExpenseDate date={props.date}></ExpenseDate>
+    <ExpenseDate date={props.date}/>
     <div className="expense-item__description">
       <h2>{props.title}</h2>
       <div className="expense-item__price">Rs. {props.amount}</div>
